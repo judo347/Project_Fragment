d569d36209df899b0f460988c669f34a4dc27752
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2018-04-14 11:38:05 +0200

    deleted sout

--- rootGame/core/src/helpers/EntityType.java
+++ rootGame/core/src/helpers/EntityType.java
@@ -80,14 +80,10 @@ public enum EntityType {
      *  @return a entityType based on the color given. Returns null if non matches. */
     public static EntityType getTypeFromColor(Color color){
 
-        if(color.equals(EntityType.PLAYER.getColor())){
-            System.out.println("Type = PLAYER");
+        if(color.equals(EntityType.PLAYER.getColor()))
             return PLAYER;
-        }
-        if(color.equals(EntityType.CHEST.getColor())){
-            System.out.println("Type = chest");
+        if(color.equals(EntityType.CHEST.getColor()))
             return CHEST;
-        }
         else
             return null;
     }
