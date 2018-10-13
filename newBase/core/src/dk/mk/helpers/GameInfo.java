30ac00c167087ba8fbe0a497ae7e7675aa1b59b7
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2018-10-14 00:36:52 +0200

    removed newBase

--- newBase/core/src/dk/mk/helpers/GameInfo.java
+++
@@ -1,9 +0,0 @@
-package dk.mk.helpers;
-
-public class GameInfo {
-
-    public static final int WIDTH = 1280;
-    public static final int HEIGHT = 800;
-    public static final float PPM = 100; //100px = 1m //PPM pixels per min //THIS ONE HAS TO BE A FLOAT!
-    public static final int TILE_SIZE = 32;
-}
